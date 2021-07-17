f8b554462fe0d36454be244ead26e5846cd7e6df
am66012 <am66012@gmail.com>
2021-07-17 23:47:37 +0530

    aded method names

--- src/tools/crudJson.js
+++ src/tools/crudJson.js
@@ -1,8 +1,22 @@
 import jsonDbApi from "../api/jsonDbApi";
 
+
+//List of Unique pageName
+
+
+//Details by PageName/URL
 export const getMetricsByPageName = async (formValues) => {
   const response = await (
     await jsonDbApi.get(`/metrics?url=${formValues}`)
   ).data;
   return response;
 };
+
+
+//Details in Selected Range and URL
+
+//Add captured Data
+
+
+
+
