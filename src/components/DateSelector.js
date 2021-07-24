6cafb9aba87ddd01217415abfd400c8e56c35808
am66012 <am66012@gmail.com>
2021-07-24 14:10:17 +0530

    Date added

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import '../App.css'
 
 class DateSelection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
 
-        };
+    handleStartDate = (event) => {
+        this.props.startDate(event.target.value);
+    }
+
+    handleEndDate = (event) => {
+        this.props.endDate(event.target.value);
     }
 
     render() {
-        // const dataPoint = this.state.metricList?.map((entry) => {
         return (
-            <div className="date-selector">
-                <input type="date"></input>
+            <div id="date_selector">
+                <div className="date-selector">
+                    <input type="date" onChange={this.handleStartDate}></input>
+                </div>
+                <div className="date-selector">
+                    <input type="date" onChange={this.handleEndDate}></input>
+                </div>
             </div>
         );
-        // });
-        // return <div>{dataPoint}</div>;
     }
 }
 
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,13 +1,40 @@
 import React from "react";
 import DateSelection from "./DateSelector";
 
+
+
 class Selection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: "",
+      endDate: ""
+    }
+  }
+
+
+  getStartDate = (value) => {
+    this.setState({
+      startDate: value
+    })
+  }
+
+  getEndDate = (value) => {
+    this.setState({
+      endDate: value
+    })
+  }
+
+
   render() {
+
+    // console.log(this.state.startDate);
+    // console.log(this.state.endDate);
     return <div>
       <h1 className="title">Core Web Vital Trends</h1>
-      <div id="date_selector">
-        <DateSelection ></DateSelection>
-        <DateSelection ></DateSelection>
+      <div >
+        <DateSelection startDate={this.getStartDate} endDate={this.getEndDate}></DateSelection>
+
       </div>
 
     </div>;
