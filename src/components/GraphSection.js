90314183d7bd75b6b100a53d8547df82b26f1574
Sushant Kumar Thakur <sushant.thakur40795@gmail.com>
2021-08-01 23:31:18 +0530

    fixed annotation errors

--- src/components/GraphSection.js
+++ src/components/GraphSection.js
@@ -16,31 +16,27 @@ class GraphSection extends React.Component {
     });
   }
 
-
-
-  dataPoint = () => this.state.metricList?.map((entry) => {
-    return (
-      <div key={entry.id}>
-        <p>{entry.id}</p>
-        <p>{entry.url}</p>
-        <p>{entry.blogCreator}</p>
-      </div>
-    );
-  });
-
+  dataPoint = () =>
+    this.state.metricList?.map((entry) => {
+      return (
+        <div key={entry.id}>
+          <p>{entry.id}</p>
+          <p>{entry.url}</p>
+          <p>{entry.blogCreator}</p>
+        </div>
+      );
+    });
 
   getDateRange = (value) => {
     let resultResponse = this.state.metricList.filter((obj) => {
       return obj.auditDate >= value.startDate && obj.auditDate <= value.endDate;
-    })
+    });
     console.log(resultResponse);
-  }
-
+  };
 
   render() {
-
     return (
-      <div class="selection_area">
+      <div className="selection_area">
         <Selection dateRange={this.getDateRange} />
         {/* {this.dataPoint} */}
       </div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,42 +1,45 @@
 import React from "react";
 import DateSelection from "./DateSelector";
 
-
 class Selection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       startDate: "",
-      endDate: ""
-    }
+      endDate: "",
+    };
   }
-
 
   getStartDate = (value) => {
     this.setState({
-      startDate: value
-    })
-  }
+      startDate: value,
+    });
+  };
 
   getEndDate = (value) => {
     this.setState({
-      endDate: value
-    })
-  }
+      endDate: value,
+    });
+  };
 
   sendDateRange = () => {
     this.props.dateRange(this.state);
-  }
-
+  };
 
   render() {
-    return <div class="selection_sub">
-      {/* <h1 className="title">Core Web Vital Trends</h1> */}
+    return (
+      <div className="selection_sub">
+        {/* <h1 className="title">Core Web Vital Trends</h1> */}
 
-      <DateSelection startDate={this.getStartDate} endDate={this.getEndDate}></DateSelection>
-      <button id="submit-button" onClick={this.sendDateRange}>FILTER</button>
-
-    </div>;
+        <DateSelection
+          startDate={this.getStartDate}
+          endDate={this.getEndDate}
+        ></DateSelection>
+        <button id="submit-button" onClick={this.sendDateRange}>
+          FILTER
+        </button>
+      </div>
+    );
   }
 }
 
